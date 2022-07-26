b26d119e7f9951ceb4411c894f65cf278ed24602
Leonid Yatskevich <53173112+leonidya@users.noreply.github.com>
2022-07-26 16:44:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+coin_side = random.randint(0,1)
+if coin_side == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+
+
+
+
+
+
+
+
